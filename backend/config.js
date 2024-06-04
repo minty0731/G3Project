d7591825ecf7efde170278310af698f79b73f8ab
minty0731 <truongminh3107@gmail.com>
2024-06-04 21:28:43 +0700

    first commit set up

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://veganadmin:vegetarianadmin@veganreviewer.lluobc1.mongodb.net/vegan-store?retryWrites=true&w=majority&appName=VeganReviewer'
